6a5c3889f27d3b2da110c8d72bbab743f9750c52
ernestnzx <ernestngzixuan@gmail.com>
2025-02-11 17:45:13 +0800

    Change formula used in ling.py

--- stronghold/ling.py
+++ stronghold/ling.py
@@ -1,7 +1,7 @@
 from ds import sa_lcp
 s = open('./testcase/rosalind_ling.txt','r').readline().strip()+'$'
-n = len(s)
+n = len(s)-1 # Account for sentinal character
 sa = sa_lcp.suffix_array_construction(s)
 lcp = sa_lcp.lcp_construction(s,sa)
-sub,m = n*(n-1)//2 - sum(lcp), sum([min(4**i,n-i) for i in range(1,n)])
+sub,m = n*(n+1)//2 - sum(lcp), sum([min(4**i,n-i+1) for i in range(1,n+1)])
 print(round(sub/m,5))
